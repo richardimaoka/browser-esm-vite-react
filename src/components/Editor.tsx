f787d84e5f2f17265d19553d9b927cb90e8020c9
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-10-08 23:13:23 +0900

    tweaking

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,5 +1,6 @@
 import { VFC, useRef, useState, useEffect } from "react";
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
+
 import styles from "./Editor.module.css";
 
 export const Editor: VFC = () => {
@@ -12,12 +13,46 @@ export const Editor: VFC = () => {
       setEditor((editor) => {
         if (editor) return editor;
 
-        return monaco.editor.create(monacoEl.current!, {
+        const createdEditor = monaco.editor.create(monacoEl.current!, {
           value: ["function x() {", '\tconsole.log("Hello world!");', "}"].join(
             "\n"
           ),
           language: "typescript",
         });
+
+        const contentWidget = {
+          getId: function () {
+            return "my.content.widget";
+          },
+          getDomNode: function () {
+            const domNode = document.createElement("div");
+            domNode.innerHTML = "My content widget";
+            domNode.style.background = "grey";
+            domNode.style.opacity = "0.6";
+            domNode.style.width = "500px";
+            domNode.style.height = `${50}px`;
+            domNode.hidden = true;
+            return domNode;
+          },
+          getPosition: function (): monaco.editor.IContentWidgetPosition {
+            return {
+              position: {
+                lineNumber: 1,
+                column: 20,
+              },
+              range: {
+                startLineNumber: 1,
+                startColumn: 10,
+                endLineNumber: 3,
+                endColumn: 10,
+              },
+              preference: [monaco.editor.ContentWidgetPositionPreference.EXACT],
+            };
+          },
+        };
+        createdEditor.addContentWidget(contentWidget);
+
+        return createdEditor;
       });
     }
 
